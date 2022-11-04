67cd51b13cb48c91c30f4e21490efe3ae2ea88fa
Sandeep7one <sandeep.y@thinkwik.com>
2022-11-04 18:48:48 +0530

    [fix] error

---
+++ src/@types/lists.ts
@@ -0,0 +1,6 @@
+export interface lists{
+    userID: string;
+    displayName: string;
+    picture: string;
+    score: number;
+}
--- src/Strimer.tsx
+++ src/Strimer.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
-import jetDevs from "./dataMock/MockData.json";
+import { lists } from "./@types/lists";
+import jetDevs from "./MockData/MockData.json";
 import {
   Container,
   ContainerLeft,
@@ -12,12 +13,37 @@ import {
 } from "./style";
 
 interface IAppData {}
+
 const Strimer: React.FunctionComponent<IAppData> = () => {
+  const [lists, setLists] = React.useState(jetDevs);
+
+  const swipingList = () => {
+    let arrays: any = lists.length,
+      temp,
+      index;
+    while (arrays > 0) {
+      index = Math.floor(Math.random() * arrays);
+      arrays--;
+
+      // Swiping Lists
+      temp = lists[arrays];
+      lists[arrays] = arrays[index];
+      arrays[index] = temp;
+    }
+    return lists;
+  };
+  // const arraySwiping = swipingList(jetDevs);
+  // setLists(arraySwiping)
+  // React.useEffect(() => {
+  //   // swipingList()
+  //   // const arraySwiping = swipingList(jetDevs);
+  //   setLists(jetDevs);
+  // }, []);
   return (
     <Container>
-      {jetDevs.map((jetDev) => {
+      {lists.map((jetDev) => {
         return (
-          <ContainerUser>
+          <ContainerUser key={jetDev.userID}>
             <ContainerRight>
               <ContainerRightUserId>{jetDev.userID}</ContainerRightUserId>
               <ContainerRightUserImage src={jetDev.picture} />
@@ -26,7 +52,9 @@ const Strimer: React.FunctionComponent<IAppData> = () => {
               </ContainerRightUserName>
             </ContainerRight>
             <ContainerLeft>
-              <ContainerLeftScore>{jetDev.score}pt</ContainerLeftScore>
+              <ContainerLeftScore title="Point">
+                {jetDev.score}pt
+              </ContainerLeftScore>
             </ContainerLeft>
           </ContainerUser>
         );
